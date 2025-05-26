fed4348a47adf8a9fa0b3c91d8bf122bf71c9d34
ZeneZ0 <vladk228.sh228@yandex.ru>
2025-05-26 11:01:08 +0800

    Удаление путсых комментариев после лабы

--- SixthLaba/Emitter.cs
+++ SixthLaba/Emitter.cs
@@ -54,30 +54,7 @@ namespace lab6
                 //particle.Life -= 1;  // не трогаем
                 if (particle.Life < 0)
                 {
-                    // тоже не трогаем
-                    /*particle.Life = 20 + Particle.rand.Next(100);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;*/
-
-                    /* это убираем
-                    particle.Direction = Particle.rand.Next(360);
-                    particle.Speed = 1 + Particle.rand.Next(10);
-                    */
-
-                    /* ЭТО ДОБАВЛЯЮ, тут сброс состояния частицы */
-                    /*var direction = (double)Particle.rand.Next(360);
-                    var speed = 1 + Particle.rand.Next(10);
-
-                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
-                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);*/
-                    /* конец ЭТО ДОБАВЛЯЮ  */
-
-                    // это не трогаем
-                    /*particle.Radius = 2 + Particle.rand.Next(10);*/
-
-                    /* 
-                     * то проверяем надо ли создать частицу
-                     */
+                
                     if (particlesToCreate > 0)
                     {
                         /* у нас как сброс частицы равносилен созданию частицы */
@@ -87,19 +64,7 @@ namespace lab6
                 }
                 else
                 {
-                    /* это все убираем, тут у нас старый пересчет положения частицы в пространстве 
-                    var directionInRadians = particle.Direction / 180 * Math.PI;
-                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
-                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
-                    */
-
-                    // сделаем сначала для одной точки
-                    // и так считаем вектор притяжения к точке
-                    /*float gX = gravityPoints[0].X - particle.X;
-                    float gY = gravityPoints[0].Y - particle.Y;*/
-
-                    // и добавляем новый, собственно он даже проще становится, 
-                    // так как теперь мы храним вектор скорости в явном виде и его не надо пересчитывать
+                    
                     particle.X += particle.SpeedX;
                     particle.Y += particle.SpeedY;
 
@@ -108,16 +73,7 @@ namespace lab6
                     // каждая точка по-своему воздействует на вектор скорости
                     foreach (var point in impactPoints)
                     {
-                        /*float gX = point.X - particle.X;
-                        float gY = point.Y - particle.Y;
-
-                        // считаем квадрат расстояния между частицей и точкой r^2
-                        float r2 = (float)Math.Max(100, gX * gX + gY * gY); // ограничил
-                        float M = 100; // сила притяжения к точке, пусть 100 будет
-
-                        // пересчитываем вектор скорости с учетом притяжения к точке
-                        particle.SpeedX += (gX) * M / r2;
-                        particle.SpeedY += (gY) * M / r2;*/
+                      
 
                         point.ImpactParticle(particle);
                     }
